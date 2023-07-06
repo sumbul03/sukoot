9302b812fbd97002c8822e60f2f9f637c5a08dd5
Hafiza Sumbul Rafiq Kath <hafiza.sumbul@gaditek.local>
2023-07-06 16:48:56 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main Template File 
+ * 
+ * @package Sukoot
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme </title>
+</head>
+<body>
+    Hellow Wp
+</body>
+</html>
